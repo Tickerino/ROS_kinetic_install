fcfcf821bdaad862ea64e5866d9a2fafbc960564
Dickson Lee <dicksonlee_8016@hotmail.com>
2017-05-09 10:52:45 +0800

    Update install_ros_desktop.sh

--- install_ros_desktop.sh
+++ install_ros_desktop.sh
@@ -13,7 +13,7 @@ read
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 #Setup the keys
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1717815A3895523BAEEB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 
 #ROS Installation
 sudo apt-get -y  update
